a5dfe3b5e9b9f82c4ea0253afd2610c0f230b985
Jake Wagner <jkwagneriii@gmail.com>
2020-11-01 10:47:13 -0600

    htmlRoutues psuedo code

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,9 +2,15 @@
 
 const db = require('./models');
 
+module.exports = {
 //FUNCTIONALITY of this controller 
     //GET all the burgers
+    smashAllBurgs: function(req, res) {
+        db.Burger.findAll({}).then(function(dbBurgerData) {res.json(dbBurgerData)})
+    }
     //GET burger by id 
     //ADD burger  
     //DELETE burger
-    //UPDATE burger
+    //UPDATE burger  
+}
+
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+//HTML ROUTES /burgers
+    //ping the db for all the burger data
+    //res.render(data "burger") -->generate all the html for the client
+
+
+//html to user
+//static files     
+    //js
+        //manages our button clicks
+            //user clicks on "devour it"
+            //ajax update('/api/burger/4') ->req
+            //move that element with the id
+    //css  
